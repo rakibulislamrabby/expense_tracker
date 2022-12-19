7b9c39b5172ded7b84a941596cf815596a8906a1
Rakibulislamrabby <rakibulislamrabby64@gmai.com>
2022-12-19 22:13:35 +0600

    first commit

---
+++ src/components/InputSection.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const InputSection = () => {
+    const [date, setDate] = useState(null);
+    const [startDate, setstartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    return (
+        <div className='grid grid-cols-2 gap-4 m-6'>
+
+            <div className='grid grid-cols-2'>
+
+                <input type="text" placeholder="Expense Title" class="input input-bordered input-secondary w-4/5 max-w-xs mr-8" />
+                <input type="number" placeholder="Amount" class="input input-bordered input-secondary w-4/5 max-w-xs" />
+
+
+
+
+                <select class="select select-secondary w-4/5 max-w-xs mt-4">
+                    <option disabled selected>Expense Type</option>
+                    <option>House Bill</option>
+                    <option>Mobile Bil</option>
+                    <option>Internet Bill</option>
+                    <option>Grosary Bill</option>
+
+                </select>
+                {/* <input type="text" placeholder="Date" class="input input-bordered input-secondary w-2/5 max-w-xs ml-8" /> */}
+
+
+                <DatePicker className='input input-bordered input-secondary w-4/5 max-w-xs mt-4' placeholderText='Date' selected={date} onChange={date => setDate(date)} />
+
+
+
+
+
+            </div>
+            <div className='grid grid-cols-2 gap-2'>
+                {/* <input type="text" placeholder="Start Date" class="input input-bordered input-secondary w-2/5 max-w-xs" /> */}
+                <DatePicker placeholderText='Start Date' className='input input-bordered input-secondary w-4/5 max-w-xs' selected={startDate} onChange={date => setstartDate(date)} />
+
+                <select class="select select-secondary w-4/5 max-w-xs ml-8">
+                    <option disabled selected>Type</option>
+                    <option>Expense</option>
+                    <option>Income</option>
+
+                </select>
+
+                {/* <input type="text" placeholder="End Date" class="input input-bordered input-secondary w-2/5 max-w-xs mt-4" /> */}
+                <DatePicker className='input input-bordered input-secondary w-4/5 max-w-xs mt-4' placeholderText='End Date' selected={endDate} onChange={date => setEndDate(date)} />
+
+                <button class="btn btn-secondary btn-wide w-4/5 ml-8 mt-4">Filter</button>
+            </div>
+        </div>
+    );
+};
+
+export default InputSection;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-purple-700 text-center">
+            <a className="btn btn-ghost normal-case text-white text-xl ">Expense Tracker</a>
+        </div>
+    );
+};
+
+export default Navbar;
